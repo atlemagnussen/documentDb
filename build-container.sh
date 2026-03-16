653fac8674595c2bac30be8a840020ccc3c42f19
Atle Magnussen <atlemagnussen@gmail.com>
2026-03-16 21:47:41 +0100

    try build

--- build-container.sh
+++ build-container.sh
@@ -1,2 +1,19 @@
-podman build -f Containerfile --tag atlmag/documentdb:latest .
+podman build -f Containerfile --tag dev.logout.work/empire/docstore:latest .
 
+# example of action
+name: Build Image
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  build:
+    runs-on: docker
+    steps:
+      - uses: actions/checkout@v4
+      - run: docker version
+      - run: echo "${{ secrets.REGISTRY_PASSWORD }}" | docker login dev.logout.work -u "${{ secrets.REGISTRY_USERNAME }}" --password-stdin
+      - run: docker build -t dev.logout.work/empire/aspauth:${{ github.sha }} .
+      - run: docker push dev.logout.work/empire/aspauth:${{ github.sha }}
