b961179544e0e2c5f248686e6745644856c57bd2
Atle Magnussen <atlemagnussen@gmail.com>
2026-04-06 11:10:33 +0200

    speech

--- client/src/views/documents/speech.ts
+++ client/src/views/documents/speech.ts
@@ -1,66 +1,91 @@
-import { html, LitElement, PropertyValues } from "lit";
-import { customElement, query } from "lit/decorators.js";
-
-const synth = window.speechSynthesis;
-let voices: Array<SpeechSynthesisVoice> = [];
-
-synth.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  console.log("Voices loaded:", voices.length);
-}
+import { css, html, LitElement, nothing, PropertyValues } from "lit"
+import { customElement, query, state } from "lit/decorators.js"
 
 @customElement("speech-test")
-export class SpeechTest extends LitElement {
+export class SpeechButton extends LitElement {
+  
+  static styles = css`
+    :host {
+      display: block;
+    }
+  `
 
-  @query("form")
-  formEl?: HTMLFormElement
+  connectedCallback() {
+    super.connectedCallback();
+    window.speechSynthesis.addEventListener("voiceschanged", this.populateVoices)
+  }
+  disconnectedCallback(): void {
+    super.disconnectedCallback()
+    window.speechSynthesis.removeEventListener("voiceschanged", this.populateVoices)
+  }
 
   @query("select")
   selectEl?: HTMLSelectElement
+
+  @state()
+  voices: Array<SpeechSynthesisVoice> = [];
+
+  @state()
+  isSpeaking = false
 
   protected firstUpdated(_changedProperties: PropertyValues): void {
     this.populateVoices()
   }
 
-  populateVoices() {
-    for (const voice of voices) {
-      const option = document.createElement("option");
-      option.textContent = `${voice.name} (${voice.lang})`;
-
-      if (voice.default) {
-        option.textContent += " — DEFAULT";
-      }
-
-      option.setAttribute("data-lang", voice.lang);
-      option.setAttribute("data-name", voice.name);
-      this.selectEl?.appendChild(option);
-    }
+  populateVoices = () => {
+    this.voices = window.speechSynthesis.getVoices()
   }
 
-  onsubmit = (event: SubmitEvent) => {
-    event.preventDefault();
+  startSpeaking = () => {
 
-    const inputEl = this.formEl?.querySelector("input")
-    const utterThis = new SpeechSynthesisUtterance(inputEl!.value)
-    const selectedOption = this.selectEl?.selectedOptions[0].getAttribute("data-name")
-    const selectedVoice = voices.find((v) => v.name === selectedOption)
+    this.isSpeaking = true
+
+    const inputEl = this.renderRoot.querySelector("input")
+    const utterance = new SpeechSynthesisUtterance(inputEl!.value)
+    const selectedOption = this.selectEl!.selectedOptions[0]
+    const selectedVoice = this.voices.find((v) => v.name === selectedOption.value)
     if (!selectedVoice)
       return
     
-    utterThis.voice = selectedVoice
+    utterance.voice = selectedVoice
 
-    synth.speak(utterThis)
-    inputEl?.blur()
+    window.speechSynthesis.speak(utterance)
+  }
+  togglePauseSpeaking = () => {
+    if (window.speechSynthesis.paused) {
+      window.speechSynthesis.resume()
+    }
+    else if (window.speechSynthesis.speaking)
+      window.speechSynthesis.pause()
+  }
+  stopSpeaking = () => {
+    if (window.speechSynthesis.speaking)
+      window.speechSynthesis.cancel()
+    this.isSpeaking = false
   }
 
   render() {
     return html`
-      <form @submit=${this.onsubmit}>
-        <input type="text" value="hello world" />
-        <select></select>
-        <button type="submit">Submit</button>
-      </form>
+      <input type="text" value="hello world" />
+      <select>
+        ${this.renderVoices()}
+      </select>
+      ${this.isSpeaking ? html`
+        <button id="pausespeaking" @click=${this.togglePauseSpeaking}>Pause</button>
+        <button id="stopspeaking" @click=${this.stopSpeaking}>Stop</button>
+      ` : html`
+        <button id="startspeaking" @click=${this.startSpeaking}>Start</button>
+      `}
     `
+  }
+  renderVoices() {
+    return this.voices.map(v => {
+      return html `
+        <option value=${v.name}>
+          ${v.name} (${v.lang}) ${v.default ? "default" : nothing}
+        </option>
+      `
+    })
   }
 }
 
